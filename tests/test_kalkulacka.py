796bd3a4163d1fd1d9c0f6107217280b1294d7e6
vojtechsedlar2005 <120457138+vojtechsedlar2005@users.noreply.github.com>
2025-02-26 12:05:13 +0100

    26.02.2025

---
+++ tests/test_kalkulacka.py
@@ -0,0 +1,21 @@
+import pytest
+from src import calculator
+
+def test_add():
+    assert calculator.add(5,3) == 8
+
+def test_add_wrong():
+    assert calculator.add_wrong(5,3) == 8
+
+def test_subtract():
+    assert  calculator.subtract(5,3) == 2
+
+def test_multiply():
+    assert  calculator.multiply(5,3) == 15
+
+def test_divide():
+    assert  calculator.divide(5,3) == 2
+
+def test_multiply_wrong():
+    assert  calculator.multiply_wrong(5,3) == 15
+
