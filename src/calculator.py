f4a497b52ee9630ba0205366016f600fc1f890c4
vojtechsedlar2005 <120457138+vojtechsedlar2005@users.noreply.github.com>
2025-04-01 10:18:40 +0200

    final_pytest
    final

--- src/calculator.py
+++ src/calculator.py
@@ -1,3 +1,5 @@
+import math
+
 def add(a, b):
     return a + b
 def add_wrong(a,b):
@@ -12,3 +14,33 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero!")
     return a / b
+
+
+
+
+def log(a, b):
+    if a <= 0:
+        raise ValueError("Cannot take log of non-positive number!")
+    if b <= 0:
+        raise ZeroDivisionError("Cannot take log with non-positive base!")
+    if b == 1:
+        raise NameError("Cannot take log with base 1!")
+    return math.log(a, b)
+
+def solve_quadratic_formula(a, b, c):
+    if (
+        (type(a) != float and type(a) != int)
+        or (type(b) != float and type(b) != int)
+        or (type(c) != float and type(c) != int)
+    ):
+        raise TypeError("All coefficients must be of type float or int!")
+    if a == 0:
+        raise SyntaxError("Cannot solve quadratic formula with a = 0!")
+    if b == 5:
+        raise NameError("I don't like when b = 5!")
+    D = b**2 - 4 * a * c
+    if D < 0:
+        raise ValueError("Cannot solve quadratic formula with negative discriminant!")
+    x1 = (-b + math.sqrt(D)) / (2 * a)
+    x2 = (-b - math.sqrt(D)) / (2 * a)
+    return x1, x2
